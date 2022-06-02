7578f2dfbd9112aaac878a476710f6c230cb78fb
VSurendharsuren <surendhar.v.2019.mct@rajalakshmi.edu.in>
2022-06-02 22:27:51 +0530

    First Commit

---
+++ src/Int_Roman.java
@@ -0,0 +1,90 @@
+// valid upto 1-39;
+
+import java.util.Scanner;
+public class Int_Roman {
+    public static void main(String[] args) {
+
+        Scanner input =  new Scanner(System.in);
+        // String roman= input.nextLine();
+
+        int a = 39;
+
+        if(((int) Math.log10(a)+1) >=2){
+
+
+
+            for(int i =1;i<=a/10;i++){
+
+                ten();
+
+
+            }
+
+            converter1(a%10);
+
+
+
+        }
+
+
+
+    }
+
+    public static void converter1(int num){
+
+
+
+        if( num == 10){
+            ten();
+        }
+
+
+
+        else if (5<num && 8>=num) {
+            System.out.print("V");
+            converter2(num-5);
+
+        }
+
+        else
+        {
+            converter2(num);
+        }
+
+
+    }
+    private static void ten(){
+        System.out.print("X");
+    }
+
+    private static void converter2(int num2) {
+
+        if(num2==4){
+            System.out.print("|V");
+        }
+        else if (num2 == 5) {
+            System.out.print("V");
+        }
+        else if( num2 == 9){
+            System.out.print("|X");
+        }
+
+        else if(3>=num2 && 1<=num2){
+            int j = 0;
+            while(j != num2){
+                System.out.print("|");
+                j++;
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/find_mountain_arr.java
@@ -0,0 +1,91 @@
+public class find_mountain_arr {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,3,1};
+        int peak=peak(arr);
+
+        int search = 2;
+        int[] pos = {-1,-1};
+        pos[0] = bs_a(arr,search,peak);
+        pos[1] = bs_d(arr,search,peak+1);
+
+        System.out.println(pos[0]+"  "+pos[1]);
+
+    }
+    public static int peak(int[] arr) {
+
+        int start =0;
+        int end = arr.length-1;
+
+        while(start != end){
+
+            int mid = (start+end)/2;
+
+            if(arr[mid]>arr[mid+1]){
+                end = mid;                //this may be the possible ans...so "end = mid" ;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;// or else we can print the end also ;
+
+
+    }
+    public static int bs_a(int[] arr , int search , int end)
+    {
+
+        int start=0;
+
+
+
+        while(start<=end){
+
+            int mid = (start+end)/2;
+
+            if (search>arr[mid]){
+                start = mid + 1;
+            }
+
+            else if(arr[mid] > search){
+                end = mid -1 ;
+            }
+
+            else{
+                return mid ;
+            }
+
+        }
+        return -1;
+    }
+
+
+
+    public static int bs_d(int[] arr , int search,int start)
+    {
+
+
+        int end=arr.length-1;
+
+
+        while(start<=end){
+
+            int mid = (start+end)/2;
+
+            if (search<arr[mid]){
+                start = mid + 1;
+            }
+
+            else if(arr[mid] < search){
+                end = mid -1 ;
+            }
+
+            else{
+                return mid ;
+            }
+
+        }
+        return -1;
+    }
+
+}
