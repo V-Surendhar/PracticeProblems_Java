bd6a2babfe630a7f9cd389098ec65bb5686199ed
VSurendharsuren <surendhar.v.2019.mct@rajalakshmi.edu.in>
2022-06-05 16:25:53 +0530

    First Commit

---
+++ src/PrettyPrinting.java
@@ -0,0 +1,13 @@
+public class PrettyPrinting {
+
+    public static void main(String[] args) {
+
+        float num = 49.29142951f;
+
+        System.out.printf("formated nuumber is.......%.2f ", num);
+        System.out.print("\n");
+
+
+    }
+
+}
