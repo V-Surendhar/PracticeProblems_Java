6e52705bc1763050b3fdea201fb422d8dc902dda
VSurendharsuren <surendhar.v.2019.mct@rajalakshmi.edu.in>
2022-06-06 16:39:36 +0530

    First Commit

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String alpha = "MALAYALAM";
+
+        for (int i = 0; i < (alpha.length()) / 2; i++) {
+
+            if (alpha.charAt(i) != (alpha.charAt(alpha.length() - i - 1))) {
+
+                System.out.println("NOT A PALINDROME");
+                break;
+            }
+
+        }
+    }
+
+
+}
+
---
+++ src/PrintAlphabets.java
@@ -0,0 +1,13 @@
+public class PrintAlphabets {
+
+    public static void main(String[] args) {
+
+        String alphabet ="";
+        for(int i =0 ; i< 26;i++){
+            alphabet+=(char)('a'+i);
+        }
+        System.out.println(alphabet);
+
+
+    }
+}
