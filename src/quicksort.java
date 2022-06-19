c08698402aa14e101327c7b9e4d0984a798b9c72
VSurendharsuren <surendhar.v.2019.mct@rajalakshmi.edu.in>
2022-06-19 12:56:20 +0530

    First Commit

---
+++ src/quicksort.java
@@ -0,0 +1,70 @@
+import java.util.*;
+public class quicksort
+{
+    public static void main(String[] args) {
+
+        //quick sort using recursion
+
+        int[] arr = {2,4,3,1,5,6,7};
+        sort(arr , 0 , arr.length-1);
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+    }
+
+    private static void sort(int[] arr , int low , int high){
+
+
+        int s = low ;
+        int e = high ;
+
+        int mid = (s+e)/2;
+        int pivot = arr[mid];
+
+        int i=0;
+        int j=0;
+        // making the pivot the stand in correct position .
+
+        while(s<=e) {
+            while(arr[s]<pivot){
+                s++;
+            }
+
+            while(arr[e]>pivot){
+                e--;
+            }
+
+            if(s<=e){
+
+                swap(arr , s , e);
+                s++;
+                e--;
+            }
+
+        }
+
+
+        if(low >= high){
+            return;
+        }
+
+        sort(arr , low , e);
+        sort(arr,s , high);
+
+
+
+
+    }
+
+    public static void swap(int[] arr , int first , int second){
+
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second]=temp;
+
+    }
+
+}
